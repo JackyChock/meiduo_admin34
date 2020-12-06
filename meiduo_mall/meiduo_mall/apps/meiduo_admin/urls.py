0a4038804c1403fee3a31e8b882fa01737def8fe
JackyChock <272601409@qq.com>
2020-12-06 17:03:30 +0800

    用户总量统计

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
+from .views.statistical import UserTotalCountView
 
 urlpatterns = [
     path('authorizations/', obtain_jwt_token),
+    # ------------  数据统计  ------------
+    path('statistical/total_count/', UserTotalCountView.as_view()),
+
 ]
