505af9ea13879d36df415af14017f99a3816839f
JackyChock <272601409@qq.com>
2020-12-07 11:40:52 +0800

    获取多个用户信息和改写分页返回结果方法

--- meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
+++ meiduo_mall/meiduo_mall/apps/meiduo_admin/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from rest_framework_jwt.views import obtain_jwt_token
 from .views.statistical import UserTotalCountView, UserDayCountView, UserActiveCountView, UserOrderCountView, \
     UserMonthCountView, GoodsDayView
+from .views.users import UserView
 
 urlpatterns = [
     path('authorizations/', obtain_jwt_token),
@@ -13,5 +14,7 @@ urlpatterns = [
     path('statistical/day_orders/', UserOrderCountView.as_view()),
     path('statistical/month_increment/', UserMonthCountView.as_view()),
     path('statistical/goods_day_views/', GoodsDayView.as_view()),
+    # ------------  用户管理  ------------
+    path('users/', UserView.as_view()),
 
 ]
